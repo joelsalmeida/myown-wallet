d3ea33b06977893d014cc55a0369a9bb23835809
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-01-10 19:40:29 -0300

    Feature - App: Adiciona Provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import store from './store';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/carteira" component={ Wallet } />
-    </Switch>
+    <Provider store={ store }>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
+    </Provider>
   );
 }
 
