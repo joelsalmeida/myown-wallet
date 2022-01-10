0d4031fc0d31d52a9725f4cc10665bdfff463221
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-01-10 16:41:09 -0300

    Feature - Login: Cria estados, cria validaçao de email e senha e adiciona handlers

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,55 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      isEmailValid: false,
+      isPasswordValid: false,
+    };
+  }
+
+  checkEmailAndPassword = () => {
+    const { email, password } = this.state;
+    const minimumLength = 6;
+
+    if (password.length >= minimumLength) {
+      this.setState({ isPasswordValid: true });
+    } else {
+      this.setState({ isPasswordValid: false });
+    }
+
+    const validEmail = 'alguem@alguem.com';
+
+    if (email === validEmail) {
+      this.setState({ isEmailValid: true });
+    } else {
+      this.setState({ isEmailValid: false });
+    }
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value },
+      () => this.checkEmailAndPassword());
+  }
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <main>
         <label htmlFor="input-email">
           Email:
           <input
             type="email"
-            name="input-email"
+            name="email"
             id="input-email"
+            onChange={ this.handleChange }
+            value={ email }
             data-testid="email-input"
           />
         </label>
@@ -18,8 +58,10 @@ class Login extends React.Component {
           Senha:
           <input
             type="password"
-            name="input-password"
+            name="password"
             id="input-password"
+            onChange={ this.handleChange }
+            value={ password }
             data-testid="password-input"
           />
         </label>
