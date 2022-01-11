9541cb359f92ee6ee652c4b9e764cb1efddd7662
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-01-11 10:04:39 -0300

    Feature - Header: Cria componente "Header" com o campo de email retirado do estado global da aplicacao. Insere componente "Header" na pagina "Wallet".

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{email}</span>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email });
+
+export default connect(mapStateToProps)(Header);
